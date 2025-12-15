f93e0e40ec9299f4965c6fc89589f676cf4776ea
Jens <jenscp171@gmail.com>
2025-12-15 12:16:15 +0100

    wrote unit test for booking manager

--- src/test/unit/booking_manager/test_booking_manager_occupied_dates.py
+++ src/test/unit/booking_manager/test_booking_manager_occupied_dates.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
+from typing import List
 from unittest.mock import AsyncMock, Mock
 import pytest
 from src.domain import entities
 from src.test.unit.booking_manager.booking_manger_parametrize_data import FUTURE_DATES_RESERVED, START_DATE_IN_FUTURE, \
-    START_DATE_HIGHER_THAN_END_DATE, START_DATE_NOT_IN_FUTURE
+    START_DATE_HIGHER_THAN_END_DATE
 from src.use_cases import BookingManager
 
 
@@ -51,45 +52,53 @@ def booking_manager(room_repository, booking_repository) -> BookingManager:
 @pytest.mark.parametrize(
     "start_date, end_date", FUTURE_DATES_RESERVED
 )
-async def test_create_booking__fully_occupied__false(start_date, end_date, booking_repository, booking_manager):
-    _booking = entities.Booking(start_date=start_date, end_date=end_date, is_active=False, customer_id=None, room_id=None, id=None)
+async def test__fully_occupied__dates_in_range__list_of_dates(start_date, end_date, booking_repository, room_repository, booking_manager):
+    _result = await booking_manager.get_fully_ocupied_dates(start_date=start_date, end_date=end_date)
 
-    result = await booking_manager.create_booking(_booking)
+    assert len(_result) is not 0
+    assert _result.__contains__(start_date.date())
+    assert room_repository.get_all_async.call_count == 1
+    assert booking_repository.get_all_async.call_count == 1
 
-    assert result == False
+@pytest.mark.asyncio
+async def test__fully_occupied_dates__inside_outside_date_in_range__dates_in_range(booking_repository, room_repository, booking_manager):
+    _expected = [
+        (datetime.now() + timedelta(days=19)).date(),
+        (datetime.now() + timedelta(days=20)).date(),
+    ]
+
+    _start_date = datetime.now() + timedelta(days=19)
+    _end_date = datetime.now() + timedelta(days=30)
+
+    _result = await booking_manager.get_fully_ocupied_dates(start_date=_start_date, end_date=_end_date)
+
+
+    assert len(_result) is not 0
+    assert _result == _expected
+    assert _result.__contains__(_start_date.date())
+    assert room_repository.get_all_async.call_count == 1
     assert booking_repository.get_all_async.call_count == 1
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "start_date, end_date", START_DATE_IN_FUTURE
 )
-async def test__create_booking__not_fully_occupied__true(start_date, end_date, booking_repository, booking_manager):
-    _booking = entities.Booking(start_date=start_date, end_date=end_date, is_active=False, customer_id=None,
-                                room_id=None, id=None)
+async def test__fully_occupied_dates__outside_date_range__empty(start_date, end_date, booking_repository, room_repository, booking_manager):
 
-    result = await booking_manager.create_booking(_booking)
+    _result = await booking_manager.get_fully_ocupied_dates(start_date=start_date, end_date=end_date)
 
-    assert result == True
+    assert len(_result) is 0
+    assert room_repository.get_all_async.call_count == 1
     assert booking_repository.get_all_async.call_count == 1
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "start_date, end_date", START_DATE_HIGHER_THAN_END_DATE
 )
-async def test__create_booking__start_date_higher_than_end_date__exception(start_date, end_date, booking_repository, booking_manager):
-    _booking = entities.Booking(start_date=start_date, end_date=end_date, is_active=False, customer_id=None,
-                                room_id=None, id=None)
-    with pytest.raises(ValueError):
-        await booking_manager.find_available_room(start_date=start_date, end_date=end_date)
-    assert booking_repository.call_count == 0
+async def test_dully_occupied_dates__dates_in_past__exception(start_date, end_date, booking_repository, room_repository, booking_manager):
 
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "start_date, end_date", START_DATE_NOT_IN_FUTURE
-)
-async def test__create_booking__start_date_biger_than_end_Date__exception(start_date, end_date, booking_repository, room_repository, booking_manager):
-    _booking = entities.Booking(start_date=start_date, end_date=end_date, is_active=False, customer_id=None,
-                                room_id=None, id=None)
     with pytest.raises(ValueError):
-        await booking_manager.find_available_room(start_date=start_date, end_date=end_date)
+        await booking_manager.get_fully_ocupied_dates(start_date=start_date, end_date=end_date)
+
+    assert room_repository.get_all_async.call_count == 0
+    assert booking_repository.get_all_async.call_count == 0
