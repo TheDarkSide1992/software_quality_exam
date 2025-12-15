d068edb551524b4f6bfc44ebd9551503faafee0a
Jens <jenscp171@gmail.com>
2025-12-15 09:25:54 +0100

    init parameterrized testing

--- src/test/unit/booking_manager_tests.py
+++ src/test/unit/booking_manager_tests.py
@@ -5,6 +5,7 @@ import pytest
 from src.domain import entities
 from src.use_cases import BookingManager
 
+# ================================= SET-UP =================================
 
 @pytest.fixture
 def room_repository():
@@ -37,11 +38,21 @@ def booking_manager(room_repository, booking_repository) -> BookingManager:
     return  BookingManager(room_repository=room_repository, booking_repository=booking_repository)
 
 
+# ================================= TEST =================================
 
 @pytest.mark.asyncio
-async def test__find_available_rooms__not_none(booking_manager, room_repository, booking_repository):
-    _start_date = datetime.now() + timedelta(days=1)
-    _end_date = datetime.now() + timedelta(days=10)
+@pytest.mark.parametrize(
+    "start_date, end_date",
+    [
+        (datetime.now() + timedelta(days=1), datetime.now() + timedelta(days=10)),
+        (datetime.now() + timedelta(days=2), datetime.now() + timedelta(days=5)),
+        (datetime.now() + timedelta(days=20), datetime.now() + timedelta(days=22)),
+        (datetime.now() + timedelta(days=32), datetime.now() + timedelta(days=35)),
+    ]
+)
+async def test__find_available_rooms__not_none(start_date,end_date,booking_manager, room_repository, booking_repository):
+    _start_date = start_date
+    _end_date = end_date
 
     result = await booking_manager.find_available_room(start_date=_start_date, end_date=_end_date)
     assert result is not None
